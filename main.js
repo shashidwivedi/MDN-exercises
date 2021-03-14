9873a92c396c0adc50f0a40e930e5e78bf342924
Shashi Dwivedi <er.shashi.dwivedi@gmail.com>
2021-03-14 23:26:43 +0530

    Make stop button function as desired

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ const clock = document.querySelector(".clock");
 
 let createClock;
 let startTime;
+let milliseconds = 0;
+let reset = true;
+let stopTime;
+let restartTime;
 
 function displayTime() {
-    let time = Date.now() - startTime;
-    let seconds = time / 1000;
+    milliseconds = Date.now() - startTime;
+    let seconds = milliseconds / 1000;
     let hours = Math.floor(seconds / 3600);
     seconds = seconds % 3600;
     let minutes = Math.floor(seconds / 60);
@@ -23,19 +27,29 @@ function formatDigit(digit) {
 }
 
 startBtn.addEventListener("click", function () {
+    clearInterval(createClock);
+    restartTime = Date.now();
     startBtn.disabled = true;
-    startTime = Date.now();
+    if (reset === true) {
+        startTime = Date.now();
+    } else {
+        startTime += (restartTime - stopTime);
+    }
     displayTime();
     createClock = setInterval(displayTime, 1000);
 });
 
 stopBtn.addEventListener("click", function () {
+    stopTime = Date.now();
     clearInterval(createClock);
+    reset = false;
     startBtn.disabled = false;
 });
 
 resetBtn.addEventListener('click', function () {
     clearInterval(createClock);
+    reset = true;
+    milliseconds = 0;
     clock.textContent = "00:00:00";
     startBtn.disabled = false;
 });
